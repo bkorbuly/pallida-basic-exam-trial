4fca7038257b189fc1494256b1ed26639020c899
bkorbuly <b.korbuly@gmail.com>
2017-09-18 15:49:14 +0200

    add and remove feature added

---
+++ dictionary/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static Dictionary<string, string> Dictionary;
+
+        static void Main(string[] args)
+        {
+            Dictionary = new Dictionary<string, string>
+            {
+                { "alma", "apple" },
+                { "fa", "tree" }
+            };
+            AddWord("auto", "car");
+            foreach (KeyValuePair<string, string> words in Dictionary)
+            {
+                Console.WriteLine("{0}, {1}", words.Key, words.Value);
+            }
+            RemoveWord("auto");
+            foreach (KeyValuePair<string, string> words in Dictionary)
+            {
+                Console.WriteLine("{0}, {1}", words.Key, words.Value);
+            }
+            Console.ReadLine();
+        }
+        public static void AddWord(string hungarianWord, string englishWord)
+        {
+            Dictionary.Add(hungarianWord, englishWord);
+        }
+        public static void RemoveWord(string hungarianWord)
+        {
+            Dictionary.Remove(hungarianWord);
+        }
+
+    }
+}
