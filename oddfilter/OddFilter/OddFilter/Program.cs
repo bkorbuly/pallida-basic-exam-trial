134891b6a78352dcfe7aae896ebaf6e8bdf5c1ac
bkorbuly <b.korbuly@gmail.com>
2017-09-18 14:47:14 +0200

    List type declared

---
+++ oddfilter/OddFilter/OddFilter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
+            PrintList(oddElements);
+            Console.ReadLine();
+        }
+        public static List<int> OddFilter(List<int> originalList)
+        {
+            List<int> newList = new List<int> ();
+            for (int i = 0; i < originalList.Count; i++)
+            {
+                if (originalList[i] % 2 != 0)
+                {
+                    newList.Add(originalList[i]);
+                }
+            }
+
+            return newList;
+        }
+        private static void PrintList(List<int> list)
+        {
+            foreach (int element in list)
+            {
+                Console.Write("{0} ", element);
+            }
+        }
+    }
+}
